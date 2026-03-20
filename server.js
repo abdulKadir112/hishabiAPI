a318939f2fbda3de1f3cfc062abfa5f08087a90a
Abdul Kadir <abdulkadir112k@gmail.com>
2026-03-20 15:40:18 +0600

    tisha

--- server.js
+++ server.js
@@ -7,19 +7,19 @@ const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 
 // Middleware
-app.use(cors({
-  origin: [
-    'http://localhost:3000',           
-    'http://localhost:5000',           
-    'https://hishabi-api.vercel.app', 
-    '*'                               
-  ],
-  methods: ['GET', 'POST', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type'],
-  credentials: true
-}));
+// app.use(cors({
+//   origin: [
+//     'http://localhost:3000',           
+//     'http://localhost:5000',           
+//     'https://hishabi-api.vercel.app', 
+//     '*'                               
+//   ],
+//   methods: ['GET', 'POST', 'DELETE', 'OPTIONS'],
+//   allowedHeaders: ['Content-Type'],
+//   credentials: true
+// }));
 
-
+app.use(cors)
 
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
